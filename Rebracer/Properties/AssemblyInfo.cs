4005750c2ea51e7dd3a7b99ba800c01177e3cf6d
Schabse Laks <Dev@SLaks.net>
2013-12-13 15:24:07 -0500

    Add initial template
    Includes slight cleanup from VSPackage wizard

---
+++ Rebracer/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Rebracer")]
+[assembly: AssemblyDescription("Saves editor formatting settings as part of each solution.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SLaks")]
+[assembly: AssemblyProduct("Rebracer")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]   
+[assembly: ComVisible(false)]     
+[assembly: CLSCompliant(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+
+[assembly: InternalsVisibleTo("Rebracer.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100eff7b7a9caef7eda298878fb78d01283d8f81b1097521172310faea7da81fc318a2a7e9a03ea9193e5180083961901602dc4b392bdd967002bec00d5356bbdd8b9a5e6c01708e51ce8323a00010358c25426e806c24a86f56cb00d0fa4646b88150c406ae2e5bef99fa177ce78a3785436662dd6176ac8cae4f7dc753968fcbe")]
