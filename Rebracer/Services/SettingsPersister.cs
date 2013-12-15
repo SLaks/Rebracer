3e1816adfdb47fe607e321e6981dda33f8df1242
Schabse Laks <Dev@SLaks.net>
2013-12-14 22:57:04 -0500

    Implement most of core services

---
+++ Rebracer/Services/SettingsPersister.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+using SLaks.Rebracer.Utilities;
+
+namespace SLaks.Rebracer.Services {
+	///<summary>Loads and saves Visual Studio settings to an XML file.</summary>
+	[Export]
+	public class SettingsPersister {
+		private readonly DTE dte;
+		private readonly ILogger logger;
+
+		[ImportingConstructor]
+		public SettingsPersister(SVsServiceProvider sp, ILogger logger) {
+			this.logger = logger;
+			dte = (DTE)sp.GetService(typeof(DTE));
+		}
+
+		///<summary>Gets or sets the path to the XML file containing the persisted settings.</summary>
+		public string SettingsPath { get; set; }
+
+		public void SaveSettings() {
+			var xml = XDocument.Load(SettingsPath);
+
+			foreach (var containerElem in xml.Root.Elements("ToolsOptions").Elements("ToolsOptionsCategory").Elements("ToolsOptionsSubCategory")) {
+				string category = containerElem.Parent.Attribute("name").Value;
+				string subcategory = containerElem.Attribute("name").Value;
+				var container = dte.Properties[category, subcategory];
+
+				XmlMerger.MergeElements(
+					containerElem,
+					container.Cast<Property>().Select(XmlValue),
+					x => x.Attribute("name").Value
+				);
+			}
+		}
+		static XElement XmlValue(Property prop) {
+			if (prop.Value is Array) {
+				return new XElement("PropertyValue",
+					new XAttribute("name", prop.Name),
+					new XAttribute("ArrayType", "VT_VARIANT"),
+					new XAttribute("ArrayElementCount", ((ICollection)prop.Value).Count),
+					((IEnumerable<object>)prop.Value)
+						.Select((v, i) => new XElement("PropertyValue", new XAttribute("name", i), v))
+				);
+			}
+			else
+				return new XElement("PropertyValue", new XAttribute("name", prop.Name), prop.Value);
+		}
+
+		///<summary>Reads settings from the XML file into Visual Studio's global settings.</summary>
+		public void LoadSettings() {
+			var xml = XDocument.Load(SettingsPath);
+
+			foreach (var subcategoryElem in xml.Root.Elements("ToolsOptions").Elements("ToolsOptionsCategory").Elements("ToolsOptionsSubCategory")) {
+				string category = subcategoryElem.Parent.Attribute("name").Value;
+				string subCategory = subcategoryElem.Attribute("name").Value;
+
+				if (!KnownSettings.IsAllowed(category, subCategory)) {
+					logger.Log("Warning: Not loading unsafe category " + category + "/" + subCategory + ".  You may have a malicious Rebracer.xml file");
+					continue;
+				}
+
+				var container = dte.Properties[category, subCategory];
+
+				foreach (var property in subcategoryElem.Elements("PropertyValue")) {
+					container.Item(property.Attribute("Name").Value).Value = VsValue(property);
+				}
+			}
+		}
+		static object VsValue(XElement elem) {
+			if (elem.Elements().Any())
+				return elem.Elements().Select(x => x.Value).ToArray();
+			else
+				return elem.Value;
+		}
+	}
+}
---
+++ Rebracer/Utilities/KnownSettings.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLaks.Rebracer.Utilities {
+	public static class KnownSettings {
+		static readonly HashSet<string> unsafeSubcategories = new HashSet<string>(StringComparer.OrdinalIgnoreCase) {
+			// AutoSaveFile allows attackers to make users load all VS settings from a hostile network share
+			"Import and Export Settings",
+			// ProjectTemplatesLocation allows attackers to host pre-infected project templates from a hostile network share
+			"ProjectsAndSolution",
+			// HomePage could allow advertising; ViewSourceExternalProgram allows RCE
+			"WebBrowser",
+			// StartPageRSSUrl could allow advertising; also, it makes no sense for this to be per-solution
+			"Startup"
+		};
+
+		///<summary>Checks whether a settings category is safe to load from untrusted data.</summary>
+		///<returns>False if the category contains properties that could allow an attacker to do harm.</returns>
+		public static bool IsAllowed(string category, string subcategory) {
+			return !unsafeSubcategories.Contains(subcategory);
+		}
+
+		///<summary>The options categories that should be included by default when creating a new settings file.</summary>
+		///<remarks>Existing files will use whatever categories exist in their XML.</remarks>
+		public static readonly IReadOnlyCollection<Tuple<string, string>> DefaultCategories = new ReadOnlyCollection<Tuple<string, string>>(new[] {
+			Tuple.Create("Environment", "TaskList"),
+			Tuple.Create("TextEditor", "CSharp Specific"),
+			Tuple.Create("TextEditor", "JavaScript Specific"),
+			Tuple.Create("TextEditor", "C/C++ Specific"),
+			Tuple.Create("TextEditor", "TypeScript Specific"),
+			Tuple.Create("TextEditor", "XAML Specific"),
+
+			//TODO: Which of these categories are used by Venus (<= Dev11) & Libra (>= Dev12)?
+			Tuple.Create("TextEditor", "HTML Specific"),
+			Tuple.Create("TextEditor", "HTMLX Specific"),
+		});
+	}
+}
