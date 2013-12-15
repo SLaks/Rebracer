59cd3b6971db27d6bcd3c5a091fa99b4fe4b0bd8
Schabse Laks <Dev@SLaks.net>
2013-12-15 12:11:00 -0500

    Implement & test XmlMerger
    Includes local copy of unreleased dennisdoomen/fluentassertions#35

---
+++ Rebracer.Tests/UtilitiesTets/XmlMergerTests.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Xml.Linq;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using FluentAssertions.Xml;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SLaks.Rebracer.Utilities;
+
+namespace Rebracer.Tests.UtilitiesTets {
+	[TestClass]
+	public class XmlMergerTests {
+		static void MergeElements(XElement container, params XElement[] newItems) {
+
+			XmlMerger.MergeElements(container, newItems, x => x.Name.LocalName);
+		}
+
+		[TestMethod]
+		public void PopulateEmptyElement() {
+			var container = new XElement("C");
+
+			MergeElements(container,
+				new XElement("c"),
+				new XElement("a"),
+				new XElement("b")
+			);
+
+			container.Should().BeEquivalentTo(new XElement("C",
+				new XElement("a"),
+				new XElement("b"),
+				new XElement("c")
+			));
+		}
+		[TestMethod]
+		public void MergeIntoElement() {
+			var container = new XElement("C",
+				new XElement("b"),
+				new XElement("d"),
+				new XElement("f")
+			);
+
+			MergeElements(container,
+				new XElement("g"),
+				new XElement("e"),
+				new XElement("b"),
+				new XElement("c"),
+				new XElement("a")
+			);
+
+			container.Should().BeEquivalentTo(new XElement("C",
+				new XElement("a"),
+				new XElement("b"),
+				new XElement("c"),
+				new XElement("d"),
+				new XElement("e"),
+				new XElement("f"),
+				new XElement("g")
+			));
+		}
+		[TestMethod]
+		public void ReorderExistingElement() {
+			var container = new XElement("C",
+				new XElement("c"),
+				new XElement("d"),
+				new XElement("b")
+			);
+
+			MergeElements(container,
+				new XElement("a"),
+				new XElement("e")
+			);
+
+			container.Should().BeEquivalentTo(new XElement("C",
+				new XElement("a"),
+				new XElement("b"),
+				new XElement("c"),
+				new XElement("d"),
+				new XElement("e")
+			));
+		}
+		[TestMethod]
+		public void StopReorderingAfterLastInsertion() {
+			var container = new XElement("C",
+				new XElement("a"),
+				new XElement("c"),
+				new XElement("e"),
+				new XElement("d")
+			);
+
+			MergeElements(container,
+				new XElement("c", new XElement("child", "Hi there!")),
+				new XElement("b")
+			);
+
+			container.Should().BeEquivalentTo(new XElement("C",
+				new XElement("a"),
+				new XElement("b"),
+				new XElement("c", new XElement("child", "Hi there!")),
+				new XElement("e"),
+				new XElement("d")
+			), "the merger should stop checking for ordering after it finishes merging all new elements");
+		}
+
+		[TestMethod]
+		public void OverwriteExistingElement() {
+			var container = new XElement("C",
+				new XElement("a"),
+				new XElement("b"),
+				new XElement("c")
+			);
+
+			MergeElements(container,
+				new XElement("a", 42),
+				new XElement("c", new XAttribute("Value", 67))
+			);
+
+			container.Should().BeEquivalentTo(new XElement("C",
+				new XElement("a", 42),
+				new XElement("b"),
+				new XElement("c", new XAttribute("Value", 67))
+			));
+		}
+	}
+	// Stolen from https://github.com/dennisdoomen/fluentassertions/pull/35
+	// TODO: Delete these classes after that is released.
+	static class XElemAssertionExtensions {
+
+		/// <summary>
+		/// Asserts that the current <see cref="XElement"/> is equivalent to the <paramref name="expected"/> element,
+		/// using its <see cref="XNode.DeepEquals()" /> implementation.
+		/// </summary>
+		/// <param name="expected">The expected element</param>
+		public static AndConstraint<XElementAssertions> BeEquivalentTo(this XElementAssertions @this, XElement expected) {
+			return @this.BeEquivalentTo(expected, string.Empty);
+		}
+
+		/// <summary>
+		/// Asserts that the current <see cref="XElement"/> is equivalent to the <paramref name="expected"/> element,
+		/// using its <see cref="XNode.DeepEquals()" /> implementation.
+		/// </summary>
+		/// <param name="expected">The expected element</param>
+		/// <param name="reason">
+		/// A formatted phrase as is supported by <see cref="string.Format(string,object[])" /> explaining why the assertion 
+		/// is needed. If the phrase does not start with the word <i>because</i>, it is prepended automatically.
+		/// </param>
+		/// <param name="reasonArgs">
+		/// Zero or more objects to format using the placeholders in <see cref="reason" />.
+		/// </param>
+		public static AndConstraint<XElementAssertions> BeEquivalentTo(this XElementAssertions @this, XElement expected, string reason, params object[] reasonArgs) {
+			Execute.Assertion
+				.ForCondition(XNode.DeepEquals(@this.Subject, expected))
+				.BecauseOf(reason, reasonArgs)
+				.FailWith("Expected XML element {0} to be equivalent to {1}{reason}.",
+					@this.Subject, expected);
+
+			return new AndConstraint<XElementAssertions>(@this);
+		}
+
+		/// <summary>
+		/// Asserts that the current <see cref="XElement"/> is not equivalent to the <paramref name="unexpected"/> element,
+		/// using its <see cref="XNode.DeepEquals()" /> implementation.
+		/// </summary>
+		/// <param name="unexpected">The unexpected element</param>
+		public static AndConstraint<XElementAssertions> NotBeEquivalentTo(this XElementAssertions @this, XElement unexpected) {
+			return @this.NotBeEquivalentTo(unexpected, string.Empty);
+		}
+
+		/// <summary>
+		/// Asserts that the current <see cref="XElement"/> is not equivalent to the <paramref name="unexpected"/> element,
+		/// using its <see cref="XNode.DeepEquals()" /> implementation.
+		/// </summary>
+		/// <param name="unexpected">The unexpected element</param>
+		/// <param name="reason">
+		/// A formatted phrase as is supported by <see cref="string.Format(string,object[])" /> explaining why the assertion 
+		/// is needed. If the phrase does not start with the word <i>because</i>, it is prepended automatically.
+		/// </param>
+		/// <param name="reasonArgs">
+		/// Zero or more objects to format using the placeholders in <see cref="reason" />.
+		/// </param>
+		public static AndConstraint<XElementAssertions> NotBeEquivalentTo(this XElementAssertions @this, XElement unexpected, string reason, params object[] reasonArgs) {
+			Execute.Assertion
+				.ForCondition(!XNode.DeepEquals(@this.Subject, unexpected))
+				.BecauseOf(reason, reasonArgs)
+				.FailWith("Did not expect XML element {0} to be equivalent to {1}{reason}.",
+					@this.Subject, unexpected);
+
+			return new AndConstraint<XElementAssertions>(@this);
+		}
+	}
+	static class XDocAssertionExtensions {
+		/// <summary>
+		/// Asserts that the current <see cref="XDocument"/> is equivalent to the <paramref name="expected"/> document,
+		/// using its <see cref="XNode.DeepEquals()" /> implementation.
+		/// </summary>
+		/// <param name="expected">The expected document</param>
+		public static AndConstraint<XDocumentAssertions> BeEquivalentTo(this XDocumentAssertions @this, XDocument expected) {
+			return @this.BeEquivalentTo(expected, string.Empty);
+		}
+
+		/// <summary>
+		/// Asserts that the current <see cref="XDocument"/> is equivalent to the <paramref name="expected"/> document,
+		/// using its <see cref="XNode.DeepEquals()" /> implementation.
+		/// </summary>
+		/// <param name="expected">The expected document</param>
+		/// <param name="reason">
+		/// A formatted phrase as is supported by <see cref="string.Format(string,object[])" /> explaining why the assertion 
+		/// is needed. If the phrase does not start with the word <i>because</i>, it is prepended automatically.
+		/// </param>
+		/// <param name="reasonArgs">
+		/// Zero or more objects to format using the placeholders in <see cref="reason" />.
+		/// </param>
+		public static AndConstraint<XDocumentAssertions> BeEquivalentTo(this XDocumentAssertions @this, XDocument expected, string reason, params object[] reasonArgs) {
+			Execute.Assertion
+				.ForCondition(XNode.DeepEquals(@this.Subject, expected))
+				.BecauseOf(reason, reasonArgs)
+				.FailWith("Expected XML document {0} to be equivalent to {1}{reason}.",
+					@this.Subject, expected);
+
+			return new AndConstraint<XDocumentAssertions>(@this);
+		}
+
+		/// <summary>
+		/// Asserts that the current <see cref="XDocument"/> is not equivalent to the <paramref name="unexpected"/> document,
+		/// using its <see cref="XNode.DeepEquals()" /> implementation.
+		/// </summary>
+		/// <param name="unexpected">The unexpected document</param>
+		public static AndConstraint<XDocumentAssertions> NotBeEquivalentTo(this XDocumentAssertions @this, XDocument unexpected) {
+			return @this.NotBeEquivalentTo(unexpected, string.Empty);
+		}
+
+		/// <summary>
+		/// Asserts that the current <see cref="XDocument"/> is not equivalent to the <paramref name="unexpected"/> document,
+		/// using its <see cref="XNode.DeepEquals()" /> implementation.
+		/// </summary>
+		/// <param name="unexpected">The unexpected document</param>
+		/// <param name="reason">
+		/// A formatted phrase as is supported by <see cref="string.Format(string,object[])" /> explaining why the assertion 
+		/// is needed. If the phrase does not start with the word <i>because</i>, it is prepended automatically.
+		/// </param>
+		/// <param name="reasonArgs">
+		/// Zero or more objects to format using the placeholders in <see cref="reason" />.
+		/// </param>
+		public static AndConstraint<XDocumentAssertions> NotBeEquivalentTo(this XDocumentAssertions @this, XDocument unexpected, string reason, params object[] reasonArgs) {
+			Execute.Assertion
+				.ForCondition(!XNode.DeepEquals(@this.Subject, unexpected))
+				.BecauseOf(reason, reasonArgs)
+				.FailWith("Did not expect XML document {0} to be equivalent to {1}{reason}.",
+					@this.Subject, unexpected);
+
+			return new AndConstraint<XDocumentAssertions>(@this);
+		}
+	}
+}
--- Rebracer/Utilities/XmlMerger.cs
+++ Rebracer/Utilities/XmlMerger.cs
@@ -7,8 +7,58 @@ using System.Xml.Linq;
 
 namespace SLaks.Rebracer.Utilities {
 	public static class XmlMerger {
+		///<summary>Merges a collection of new XML elements into an existing parent element, ensuring that the results are sorted alphabetically.</summary>
+		///<param name="container">The parent element containing the original elements to merge into.  If the merge encounters unsorted elements, the entire container will be sorted.</param>
+		///<param name="newElements">The elements to merge in.</param>
+		///<param name="nameSelector">A delegate to extract the name from an element to compare against.  Use this to sort by element name or a name attribute.</param>
+		///<remarks>
+		/// Any elements in the original that are not in <paramref name="newElements"/> will remain as-is.
+		/// Elements that exist in both will be replaced by the new element.
+		/// If the original container or <paramref name="newElements"/> have duplicate names, the behavior is undefined.
+		///</remarks>
 		public static void MergeElements(this XElement container, IEnumerable<XElement> newElements, Func<XElement, string> nameSelector) {
-			//TODO: Mergesort
+			var newItems = newElements.Select(e => new KeyValuePair<string, XElement>(nameSelector(e), e)).OrderBy(t => t.Key).ToList();
+
+			// May not be sorted
+			var oldItems = container.Elements().ToList();
+
+			int newIndex = 0;
+
+			string lastKey = null;
+			foreach (var o in oldItems) {
+				var thisKey = nameSelector(o);
+
+				// Insert any new items that should come before this element
+				while (newIndex < newItems.Count && newItems[newIndex].Key.CompareTo(thisKey) < 0) {
+					o.AddBeforeSelf(newItems[newIndex].Value);
+					newIndex++;
+				}
+
+				// If this element has a replacement in the new set, use it.
+				if(newIndex < newItems.Count && newItems[newIndex].Key.CompareTo(thisKey) == 0) {
+					o.ReplaceWith(newItems[newIndex].Value);
+					newIndex++;
+				}
+
+				// If we finished inserting all of the new elements, stop early.
+				// This means that we won't check that the rest of the container
+				// is sorted; that is fine.
+				if (newIndex == newItems.Count)
+					return;
+
+				// If the container is not already sorted, sort it,
+				// then try again.
+				if (thisKey.CompareTo(lastKey) < 0) {
+					container.ReplaceNodes(oldItems.OrderBy(nameSelector));
+					MergeElements(container, newElements, nameSelector);
+					return;
+				}
+
+				lastKey = thisKey;
+			}
+			// Add any new items that go after the last item.
+			for (; newIndex < newItems.Count; newIndex++)
+				container.Add(newItems[newIndex].Value);
 		}
 	}
 }
