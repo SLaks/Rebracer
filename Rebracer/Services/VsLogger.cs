5fd55647313a35963f4a3743ca5e9e825c9503c2
Schabse Laks <Dev@SLaks.net>
2013-12-15 23:29:37 -0500

    Fix output pane creation

--- Rebracer/Services/VsLogger.cs
+++ Rebracer/Services/VsLogger.cs
@@ -17,8 +17,14 @@ namespace SLaks.Rebracer.Services {
 		public VsLogger(SVsServiceProvider sp) {
 			var window = (IVsOutputWindow)sp.GetService(typeof(SVsOutputWindow));
 
-			var guid = VSConstants.OutputWindowPaneGuid.GeneralPane_guid;
-			ErrorHandler.ThrowOnFailure(window.GetPane(ref guid, out pane));
+			var page = VSConstants.OutputWindowPaneGuid.GeneralPane_guid;
+
+			// If the pane does not yet exist, create it
+			int hr = window.GetPane(ref page, out pane);
+			if (ErrorHandler.Failed(hr))
+				ErrorHandler.ThrowOnFailure(window.CreatePane(ref page, "General", 1, 0));
+
+			ErrorHandler.ThrowOnFailure(window.GetPane(ref page, out pane));
 		}
 
 		public void Log(string message) {
