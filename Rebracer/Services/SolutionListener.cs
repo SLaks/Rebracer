5c2ad87fe9fb320bc0a268ac8361a3518afdbe30
Schabse Laks <Dev@SLaks.net>
2013-12-15 23:57:59 -0500

    Fix DTE event handlers
    Don't let COM event objects get GC'd

--- Rebracer/Services/SolutionListener.cs
+++ Rebracer/Services/SolutionListener.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -25,6 +26,12 @@ namespace SLaks.Rebracer.Services {
 			VSConstants.VSStd97CmdID.CustomizeKeyboard
 		};
 
+		// Necessary to prevent event objects from being GC'd.
+		// See http://stackoverflow.com/a/13581371/34397
+		private readonly SolutionEvents solutionEvents;
+		private readonly DTEEvents dteEvents;
+		private readonly List<CommandEvents> commandsEvents = new List<CommandEvents>();
+
 		[ImportingConstructor]
 		public SolutionListener(SVsServiceProvider sp, ILogger logger, SettingsPersister persister, SettingsLocator locator) {
 			this.logger = logger;
@@ -33,28 +40,29 @@ namespace SLaks.Rebracer.Services {
 
 			dte = (DTE)sp.GetService(typeof(DTE));
 
-			dte.Events.DTEEvents.OnStartupComplete += DTEEvents_OnStartupComplete;
-			dte.Events.SolutionEvents.AfterClosing += SolutionEvents_AfterClosing;
-			dte.Events.SolutionEvents.Opened += SolutionEvents_Opened;
+			dteEvents = dte.Events.DTEEvents;
+			solutionEvents = dte.Events.SolutionEvents;
+			dteEvents = dte.Events.DTEEvents;
+
+			dteEvents.OnStartupComplete += DTEEvents_OnStartupComplete;
+			solutionEvents.AfterClosing += SolutionEvents_AfterClosing;
+			solutionEvents.Opened += SolutionEvents_Opened;
 
 			foreach (var optionCmdId in optionsCommands) {
-				dte.Events.CommandEvents[VSConstants.CMDSETID.StandardCommandSet97_string, (int)optionCmdId].AfterExecute += ToolsOptionsCommand_AfterExecute;
+				AddCommandEventHandler(VSConstants.GUID_VSStandardCommandSet97, optionCmdId, ToolsOptionsCommand_AfterExecute);
 			}
-			dte.Events.CommandEvents[VSConstants.CMDSETID.StandardCommandSet97_string, (int)VSConstants.VSStd97CmdID.SaveSolution].AfterExecute += SaveAllCommand_AfterExecute;
+			AddCommandEventHandler(VSConstants.GUID_VSStandardCommandSet97, VSConstants.VSStd97CmdID.SaveSolution, SaveAllCommand_AfterExecute);
 
-			dte.Events.DTEEvents.OnBeginShutdown += DTEEvents_OnBeginShutdown;
-			dte.Events.SolutionEvents.BeforeClosing += SolutionEvents_BeforeClosing;
+			dteEvents.OnBeginShutdown += DTEEvents_OnBeginShutdown;
+			solutionEvents.BeforeClosing += SolutionEvents_BeforeClosing;
 
 			var pct = (IVsRegisterPriorityCommandTarget)sp.GetService(typeof(SVsRegisterPriorityCommandTarget));
-
-			// We never unregister, so we don't need the cookie.
-			uint ignored;
-			var optionsCommandTarget = new OptionsCommandTarget(sp);
-			ErrorHandler.ThrowOnFailure(pct.RegisterPriorityCommandTarget(0, optionsCommandTarget, out ignored));
-			optionsCommandTarget.OptionsDialogClosed += OptionsCommandTarget_OptionsDialogClosed;
-			optionsCommandTarget.BeforeSaveSolution += OptionsCommandTarget_BeforeSaveSolution;
 		}
-
+		private void AddCommandEventHandler(Guid group, VSConstants.VSStd97CmdID cmdId, _dispCommandEvents_AfterExecuteEventHandler handler) {
+			var h = dte.Events.CommandEvents[group.ToString("B"), (int)cmdId];
+			h.AfterExecute += handler;
+			commandsEvents.Add(h);
+		}
 
 		#region Events to read settings
 		private void SolutionEvents_Opened() {
@@ -83,10 +91,6 @@ namespace SLaks.Rebracer.Services {
 			persister.SaveSettings();
 		}
 
-		private void OptionsCommandTarget_OptionsDialogClosed(object sender, EventArgs e) {
-			// After the user changes any options, save them.
-			persister.SaveSettings();
-		}
 		private void DTEEvents_OnBeginShutdown() {
 			// In case settings changed without the options dialog, save on exit
 			persister.SaveSettings();
@@ -97,75 +101,12 @@ namespace SLaks.Rebracer.Services {
 			if (persister.SettingsPath != locator.UserSettingsFile)
 				persister.SaveSettings();
 		}
+
 		private void SaveAllCommand_AfterExecute(string Guid, int ID, object CustomIn, object CustomOut) {
 			// In case settings changed without the options dialog, save solution settings on Save All
 			if (persister.SettingsPath != locator.UserSettingsFile)
 				persister.SaveSettings();
 		}
-		private void OptionsCommandTarget_BeforeSaveSolution(object sender, EventArgs e) {
-			// In case settings changed without the options dialog, save solution settings on Save All
-			if (persister.SettingsPath != locator.UserSettingsFile)
-				persister.SaveSettings();
-		}
 		#endregion
-
-		sealed class OptionsCommandTarget : IOleCommandTarget {
-			readonly IOleCommandTarget globalTarget;
-			public OptionsCommandTarget(SVsServiceProvider sp) {
-				globalTarget = (IOleCommandTarget)sp.GetService(typeof(SUIHostCommandDispatcher));
-			}
-
-			// We swallow the options command, then re-invoke it and raise our event
-			// after the blocking dialog is finished. This variable prevents us from
-			// catching the re-invoked command.
-			bool isReentering;
-
-			public int Exec(ref Guid pguidCmdGroup, [ComAliasName("Microsoft.VisualStudio.OLE.Interop.DWORD")]uint nCmdID, [ComAliasName("Microsoft.VisualStudio.OLE.Interop.DWORD")]uint nCmdexecopt, IntPtr pvaIn, IntPtr pvaOut) {
-				if (pguidCmdGroup != VSConstants.GUID_VSStandardCommandSet97)
-					return (int)OleConstants.OLECMDERR_E_NOTSUPPORTED;  // Skip filter
-				if (nCmdID == (uint)VSConstants.VSStd97CmdID.SaveSolution) {
-					OnBeforeSaveSolution();
-					return (int)OleConstants.OLECMDERR_E_NOTSUPPORTED;  // Skip filter
-				}
-
-				if (!optionsCommands.Contains((VSConstants.VSStd97CmdID)nCmdID))
-					return (int)OleConstants.OLECMDERR_E_NOTSUPPORTED;  // Skip filter
-
-				if (isReentering) {
-					isReentering = false;
-					return (int)OleConstants.OLECMDERR_E_NOTSUPPORTED;  // Skip filter
-				}
-
-				isReentering = true;
-				var retVal = globalTarget.Exec(pguidCmdGroup, nCmdID, nCmdexecopt, pvaIn, pvaOut);
-				OnOptionsDialogClosed();
-				return retVal;
-			}
-
-			public int QueryStatus(ref Guid pguidCmdGroup, [ComAliasName("Microsoft.VisualStudio.OLE.Interop.ULONG")]uint cCmds, [ComAliasName("Microsoft.VisualStudio.OLE.Interop.OLECMD")]OLECMD[] prgCmds, [ComAliasName("Microsoft.VisualStudio.OLE.Interop.OLECMDTEXT")]IntPtr pCmdText) {
-				return (int)OleConstants.OLECMDERR_E_NOTSUPPORTED;  // Skip filter
-			}
-
-			///<summary>Occurs when the options dialog is shown.</summary>
-			public event EventHandler OptionsDialogClosed;
-			///<summary>Raises the OptionsDialogClosed event.</summary>
-			void OnOptionsDialogClosed() { OnOptionsDialogClosed(EventArgs.Empty); }
-			///<summary>Raises the OptionsDialogClosed event.</summary>
-			///<param name="e">An EventArgs object that provides the event data.</param>
-			void OnOptionsDialogClosed(EventArgs e) {
-				if (OptionsDialogClosed != null)
-					OptionsDialogClosed(this, e);
-			}
-			///<summary>Occurs when the SaveSolution command is executed.</summary>
-			public event EventHandler BeforeSaveSolution;
-			///<summary>Raises the BeforeSaveSolution event.</summary>
-			void OnBeforeSaveSolution() { OnBeforeSaveSolution(EventArgs.Empty); }
-			///<summary>Raises the BeforeSaveSolution event.</summary>
-			///<param name="e">An EventArgs object that provides the event data.</param>
-			void OnBeforeSaveSolution(EventArgs e) {
-				if (BeforeSaveSolution != null)
-					BeforeSaveSolution(this, e);
-			}
-		}
 	}
 }
