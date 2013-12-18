180265976f31fe8b76ad7ffeba23ecb94a42a7de
Schabse Laks <Dev@SLaks.net>
2013-12-17 20:16:05 -0500

    Warn when users add a settings file elsewhere

--- Rebracer/Services/SolutionListener.cs
+++ Rebracer/Services/SolutionListener.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
+using System.IO;
+using System.Windows.Forms;
 using EnvDTE;
+using EnvDTE80;
 using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Shell;
 using Task = System.Threading.Tasks.Task;
@@ -26,6 +29,7 @@ namespace SLaks.Rebracer.Services {
 		// See http://stackoverflow.com/a/13581371/34397
 		private readonly SolutionEvents solutionEvents;
 		private readonly DTEEvents dteEvents;
+		private readonly ProjectItemsEvents projectEvents;
 		private readonly List<CommandEvents> commandsEvents = new List<CommandEvents>();
 
 		[ImportingConstructor]
@@ -43,6 +47,9 @@ namespace SLaks.Rebracer.Services {
 			solutionEvents.AfterClosing += SolutionEvents_AfterClosing;
 			solutionEvents.Opened += SolutionEvents_Opened;
 
+			projectEvents = ((Events2)dte.Events).ProjectItemsEvents;
+			projectEvents.ItemAdded += ProjectEvents_ItemAdded;
+
 			foreach (var optionCmdId in optionsCommands) {
 				AddCommandEventHandler(VSConstants.GUID_VSStandardCommandSet97, optionCmdId, ToolsOptionsCommand_AfterExecute);
 			}
@@ -51,6 +58,7 @@ namespace SLaks.Rebracer.Services {
 			dteEvents.OnBeginShutdown += DTEEvents_OnBeginShutdown;
 			solutionEvents.BeforeClosing += SolutionEvents_BeforeClosing;
 		}
+
 		private void AddCommandEventHandler(Guid group, VSConstants.VSStd97CmdID cmdId, _dispCommandEvents_AfterExecuteEventHandler handler) {
 			var h = dte.Events.CommandEvents[group.ToString("B"), (int)cmdId];
 			h.AfterExecute += handler;
@@ -79,6 +87,26 @@ namespace SLaks.Rebracer.Services {
 			// whether the used opened a solution, and
 			// activate the solution or global file.
 			persister.ActivateSettingsFile(locator.GetActiveFile(dte.Solution));
+		}
+
+		private void ProjectEvents_ItemAdded(ProjectItem ProjectItem) {
+			// If the user added a different file, or if there
+			// is no real solution, do nothing.
+			// Also do nothing when a solution that contains a
+			// Solution Item that doesn't exist is opened.
+			var expectedPath = locator.SolutionPath(dte.Solution);
+			if (String.IsNullOrEmpty(expectedPath)
+			 || ProjectItem.Name != Path.GetFileName(expectedPath)
+			 || ProjectItem.FileCount != 0 || !File.Exists(ProjectItem.FileNames[1]))
+				return;
+
+			if (File.Exists(expectedPath))
+				persister.ActivateSettingsFile(expectedPath);
+			else
+				MessageBox.Show("The Rebracer settings file that you added is not in the correct location; you are still using global settings.\n"
+							  + "To add a Rebracer settings file, right-click the solution, click Add, New Rebracer Settings File\n"
+							  + "Alternatively, copy an existing settings file to \n  " + expectedPath,
+								"Rebracer", MessageBoxButtons.OK, MessageBoxIcon.Warning);
 		}
 		#endregion
 
