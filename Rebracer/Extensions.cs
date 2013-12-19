40f101f8c475ffefa76f2b36af6fabda5cafc063
Schabse Laks <Dev@SLaks.net>
2013-12-18 23:18:31 -0500

    Notifications: Implement INotificationService

--- Rebracer/Extensions.cs
+++ Rebracer/Extensions.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
 using EnvDTE;
 using EnvDTE80;
+using Microsoft.VisualStudio.PlatformUI;
 using SLaks.Rebracer.Utilities;
 
 namespace SLaks.Rebracer {
@@ -24,7 +27,17 @@ namespace SLaks.Rebracer {
 						   .OfType<Project>()
 						   .FirstOrDefault(p => p.Name.Equals(SolutionItems, StringComparison.OrdinalIgnoreCase))
 						?? ((Solution2)solution).AddSolutionFolder(SolutionItems);
+		}
 
+		///<summary>Finds the actual bounds of a Visual Studio window in logical pixels.</summary>
+		/// <remarks>When a window is maximized, its position properties return non-maximized values.  This method works around that.</remarks>
+		public static Rect ActualBounds(this EnvDTE.Window window) {
+			if (window.WindowState != vsWindowState.vsWindowStateMaximize)
+				return new Rect(window.Left, window.Top, window.Width, window.Height).DeviceToLogicalUnits();
+
+			var screen = Screen.FromHandle(new IntPtr(window.HWnd));
+			return new Rect(screen.WorkingArea.Left, screen.WorkingArea.Top,
+							screen.WorkingArea.Width, screen.WorkingArea.Height).DeviceToLogicalUnits();
 		}
 	}
 }
---
+++ Rebracer/Notifications/GrowlNotificationsService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Windows;
+using System.Windows.Interop;
+using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+using SLaks.Rebracer.Services;
+using WPFGrowlNotification;
+
+namespace SLaks.Rebracer.Notifications {
+	[Export(typeof(INotificationService))]
+	class GrowlNotificationsService : GrowlNotifications, INotificationService {
+		private readonly DTE dte;
+
+		[ImportingConstructor]
+		public GrowlNotificationsService(SVsServiceProvider sp) {
+			dte = (DTE)sp.GetService(typeof(DTE));
+			dte.Events.DTEEvents.OnStartupComplete += DTEEvents_OnStartupComplete;
+		}
+
+		private void DTEEvents_OnStartupComplete() {
+			var wih = new WindowInteropHelper(this);
+			wih.Owner = new IntPtr(dte.MainWindow.HWnd);
+		}
+
+		private void UpdateLocation() {
+			var dteBounds = dte.MainWindow.ActualBounds();
+
+			Left = dteBounds.Right - Width - 20;
+			Top = dteBounds.Top + 20;
+		}
+
+		public void ShowNotification(string title, string text) {
+			UpdateLocation();
+			AddNotification(new Notification {
+				Title = title,
+				Message = text,
+				ImageUrl = "pack://application:,,,/Rebracer;component/Resources/Rebracer-100.png"
+			});
+		}
+	}
+}
