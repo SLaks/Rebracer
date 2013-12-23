f6807da86a813295bca2cc8b0d43e519bff436b8
Schabse Laks <Dev@SLaks.net>
2013-12-22 21:42:31 -0500

    Notifications: Add design-time theme dictionary
    This actually works:exclamation: :clap:

---
+++ Rebracer/Notifications/DesignerThemeDictionary.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Microsoft.Internal.VisualStudio.PlatformUI;
+using Microsoft.Internal.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.ComponentModelHost;
+using Microsoft.VisualStudio.Platform.WindowManagement;
+using Microsoft.VisualStudio.PlatformUI;
+using Microsoft.VisualStudio.Shell;
+
+namespace SLaks.Rebracer.Notifications {
+	public class DesignerThemeDictionary : DeferredResourceDictionaryBase {
+
+		// We must access properties from these classes using dynamic due to NoPIA conflicts.
+		// The compiler gives some errors since we do not have the right PIA, and the runtime
+		// gives more errors because NoPIA doesn't unify for managed implementations.
+		dynamic currentTheme;
+		readonly ColorThemeService service;
+		public DesignerThemeDictionary() {
+			if (ServiceProvider.GlobalProvider.GetService(typeof(SComponentModel)) != null)
+				return; // Do nothing when hosted in VS
+			ServiceProviderMock.Initialize();
+			service = (ColorThemeService)Activator.CreateInstance(typeof(ColorThemeService));
+			ThemeIndex = 0;
+		}
+		int themeIndex;
+		public int ThemeIndex {
+			get { return themeIndex; }
+			set { themeIndex = value; LoadTheme(value); }
+		}
+		public void LoadTheme(int index) {
+			if (service == null)
+				return;
+			Clear();
+
+			dynamic dynamicService = service;
+			currentTheme = dynamicService.Themes[index % service.Themes.Count];
+			foreach (ColorName colorName in dynamicService.ColorNames) {
+				IVsColorEntry vsColorEntry = currentTheme[colorName];
+				if (vsColorEntry != null) {
+					if (vsColorEntry.BackgroundType != 0) {
+						ThemeResourceKey brushKey = new ThemeResourceKey(vsColorEntry.ColorName.Category, vsColorEntry.ColorName.Name, ThemeResourceKeyType.BackgroundBrush);
+						ThemeResourceKey colorKey = new ThemeResourceKey(vsColorEntry.ColorName.Category, vsColorEntry.ColorName.Name, ThemeResourceKeyType.BackgroundColor);
+						Add(brushKey, brushKey);
+						Add(colorKey, colorKey);
+						int num = VsColorFromName(colorName);
+						if (num != 0) {
+							Add(VsColors.GetColorKey(num), colorKey);
+							Add(VsBrushes.GetBrushKey(num), brushKey);
+						}
+					}
+					if (vsColorEntry.ForegroundType != 0) {
+						ThemeResourceKey brushKey = new ThemeResourceKey(vsColorEntry.ColorName.Category, vsColorEntry.ColorName.Name, ThemeResourceKeyType.ForegroundBrush);
+						ThemeResourceKey colorKey = new ThemeResourceKey(vsColorEntry.ColorName.Category, vsColorEntry.ColorName.Name, ThemeResourceKeyType.ForegroundColor);
+						Add(brushKey, brushKey);
+						Add(colorKey, colorKey);
+					}
+				}
+			}
+		}
+
+		// Microsoft.VisualStudio.Platform.WindowManagement.ColorNameTranslator
+		static int VsColorFromName(ColorName colorName) {
+			int result;
+			if (colorName.Category == EnvironmentColors.Category && VsColors.TryGetColorIDFromBaseKey(colorName.Name, out result)) {
+				return result;
+			}
+			return 0;
+		}
+
+		protected override uint GetRgbaColorValue(ThemeResourceKey key) {
+			var entry = currentTheme[new ColorName { Category = key.Category, Name = key.Name }];
+			switch (key.KeyType) {
+				case ThemeResourceKeyType.ForegroundColor:
+				case ThemeResourceKeyType.ForegroundBrush:
+					return entry.Foreground;
+				case ThemeResourceKeyType.BackgroundColor:
+				case ThemeResourceKeyType.BackgroundBrush:
+					return entry.Background;
+				default:
+					throw new InvalidEnumArgumentException("key", (int)key.KeyType, typeof(ThemeResourceKeyType));
+			}
+		}
+	}
+}
+namespace Microsoft.Internal.VisualStudio.Shell.Interop {
+
+	[CompilerGenerated, Guid("0D915B59-2ED7-472A-9DE8-9161737EA1C5"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown), TypeIdentifier]
+	[ComImport]
+	public interface SVsColorThemeService {
+	}
+	[CompilerGenerated, Guid("EAB552CF-7858-4F05-8435-62DB6DF60684"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown), TypeIdentifier]
+	[ComImport]
+	public interface IVsColorThemeService {
+		IVsColorThemes Themes {
+			[return: MarshalAs(UnmanagedType.Interface)]
+			get;
+		}
+		IVsColorNames ColorNames {
+			[return: MarshalAs(UnmanagedType.Interface)]
+			get;
+		}
+		IVsColorTheme CurrentTheme {
+			[return: MarshalAs(UnmanagedType.Interface)]
+			get;
+		}
+		void NotifyExternalThemeChanged();
+		uint GetCurrentVsColorValue([In] int vsSysColor);
+		uint GetCurrentColorValue([In] ref Guid rguidColorCategory, [MarshalAs(UnmanagedType.LPWStr)] [In] string pszColorName, [In] uint dwColorType);
+		uint GetCurrentEncodedColor([In] ref Guid rguidColorCategory, [MarshalAs(UnmanagedType.LPWStr)] [In] string pszColorName, [In] uint dwColorType);
+	}
+	[CompilerGenerated, Guid("98192AFE-75B9-4347-82EC-FF312C1995D8"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown), TypeIdentifier]
+	[ComImport]
+	public interface IVsColorThemes {
+		IVsColorTheme this[[In] int index] {
+			[return: MarshalAs(UnmanagedType.Interface)]
+			get;
+		}
+		int Count { get; }
+		[return: MarshalAs(UnmanagedType.Interface)]
+		IVsColorTheme GetThemeFromId([In] Guid ThemeId);
+		[return: MarshalAs(UnmanagedType.CustomMarshaler, MarshalType = "System.Runtime.InteropServices.CustomMarshalers.EnumeratorToEnumVariantMarshaler")]
+		IEnumerator GetEnumerator();
+	}
+	[CompilerGenerated, Guid("92144F7A-61DE-439B-AA66-13BE7CDEC857"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown), TypeIdentifier]
+	[ComImport]
+	public interface IVsColorNames {
+		ColorName this[[In] int index] { get; }
+		int Count { get; }
+		ColorName GetNameFromVsColor([In] int vsSysColor);
+		[return: MarshalAs(UnmanagedType.CustomMarshaler, MarshalType = "System.Runtime.InteropServices.CustomMarshalers.EnumeratorToEnumVariantMarshaler")]
+		IEnumerator GetEnumerator();
+	}
+	[CompilerGenerated, Guid("413D8344-C0DB-4949-9DBC-69C12BADB6AC"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown), TypeIdentifier]
+	[ComImport]
+	public interface IVsColorTheme {
+		IVsColorEntry this[[In] ColorName Name] {
+			[return: MarshalAs(UnmanagedType.Interface)]
+			get;
+		}
+		Guid ThemeId { get; }
+		string Name {
+			[return: MarshalAs(UnmanagedType.BStr)]
+			get;
+		}
+		bool IsUserVisible { get; }
+		void Apply();
+	}
+	[CompilerGenerated, Guid("BBE70639-7AD9-4365-AE36-9877AF2F973B"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown), TypeIdentifier]
+	[ComImport]
+	public interface IVsColorEntry {
+		ColorName ColorName { get; }
+		byte BackgroundType { get; }
+		byte ForegroundType { get; }
+		uint Background { get; }
+		uint Foreground { get; }
+		uint BackgroundSource { get; }
+		uint ForegroundSource { get; }
+	}
+
+	[CompilerGenerated, TypeIdentifier("EF2A7BE1-84AF-4E47-A2CF-056DF55F3B7A", "Microsoft.Internal.VisualStudio.Shell.Interop.ColorName")]
+	[StructLayout(LayoutKind.Sequential, Pack = 8)]
+	public struct ColorName {
+		public Guid Category;
+		[MarshalAs(UnmanagedType.BStr)]
+		public string Name;
+	}
+}
---
+++ Rebracer/Notifications/ServiceProviderMock.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Settings;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VsSDK.UnitTestLibrary;
+
+namespace SLaks.Rebracer.Notifications {
+	static class ServiceProviderMock {
+		[SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope", Justification = "These objects become global and must not be disposed yet")]
+		public static void Initialize() {
+			if (ServiceProvider.GlobalProvider.GetService(typeof(SVsSettingsManager)) != null)
+				return;
+
+			var esm = ExternalSettingsManager.CreateForApplication(@"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe");
+			var sp = new OleServiceProvider();
+
+			// Used by ServiceProvider
+			sp.AddService(typeof(SVsActivityLog), new DummyLog(), false);
+			sp.AddService(typeof(SVsSettingsManager), new SettingsWrapper(esm), true);
+
+			ServiceProvider.CreateFromSetSite(sp);
+		}
+
+		class SettingsWrapper : IVsSettingsManager, IDisposable {
+			readonly ExternalSettingsManager inner;
+
+			public SettingsWrapper(ExternalSettingsManager inner) {
+				this.inner = inner;
+			}
+
+			public void Dispose() {
+				inner.Dispose();
+			}
+
+			public int GetApplicationDataFolder([ComAliasName("Microsoft.VisualStudio.Shell.Interop.VSAPPLICATIONDATAFOLDER")]uint folder, out string folderPath) {
+				folderPath = inner.GetApplicationDataFolder((ApplicationDataFolder)folder);
+				return 0;
+			}
+
+			public int GetCollectionScopes([ComAliasName("OLE.LPCOLESTR")]string collectionPath, [ComAliasName("Microsoft.VisualStudio.Shell.Interop.VSENCLOSINGSCOPES")]out uint scopes) {
+				scopes = (uint)inner.GetCollectionScopes(collectionPath);
+				return 0;
+			}
+
+			public int GetCommonExtensionsSearchPaths([ComAliasName("OLE.ULONG")]uint paths, string[] commonExtensionsPaths, [ComAliasName("OLE.ULONG")]out uint actualPaths) {
+				if (commonExtensionsPaths == null)
+					actualPaths = (uint)inner.GetCommonExtensionsSearchPaths().Count();
+				else {
+					inner.GetCommonExtensionsSearchPaths().ToList().CopyTo(commonExtensionsPaths);
+					actualPaths = (uint)commonExtensionsPaths.Length;
+				}
+
+				return 0;
+			}
+
+			public int GetPropertyScopes([ComAliasName("OLE.LPCOLESTR")]string collectionPath, [ComAliasName("OLE.LPCOLESTR")]string propertyName, [ComAliasName("Microsoft.VisualStudio.Shell.Interop.VSENCLOSINGSCOPES")]out uint scopes) {
+				scopes = (uint)inner.GetPropertyScopes(collectionPath, propertyName);
+				return 0;
+			}
+
+			public int GetReadOnlySettingsStore([ComAliasName("Microsoft.VisualStudio.Shell.Interop.VSSETTINGSSCOPE")]uint scope, out IVsSettingsStore store) {
+				store = new StoreWrapper(inner.GetReadOnlySettingsStore((SettingsScope)scope));
+				return 0;
+			}
+
+			public int GetWritableSettingsStore([ComAliasName("Microsoft.VisualStudio.Shell.Interop.VSSETTINGSSCOPE")]uint scope, out IVsWritableSettingsStore writableStore) {
+				writableStore = (IVsWritableSettingsStore)inner.GetReadOnlySettingsStore((SettingsScope)scope);
+				return 0;
+			}
+		}
+
+		class StoreWrapper : IVsSettingsStore {
+			readonly SettingsStore inner;
+
+			public StoreWrapper(SettingsStore inner) {
+				this.inner = inner;
+			}
+
+			public int CollectionExists([ComAliasName("OLE.LPCOLESTR")]string collectionPath, [ComAliasName("OLE.BOOL")]out int pfExists) {
+				pfExists = inner.CollectionExists(collectionPath) ? 1 : 0;
+				return 0;
+			}
+
+			[SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope", Justification = "No native resources are involved.")]
+			public int GetBinary([ComAliasName("OLE.LPCOLESTR")]string collectionPath, [ComAliasName("OLE.LPCOLESTR")]string propertyName, [ComAliasName("OLE.ULONG")]uint byteLength, [ComAliasName("TextManager.BYTE")]byte[] pBytes, [ComAliasName("OLE.ULONG")]uint[] actualByteLength) {
+				var stream = inner.GetMemoryStream(collectionPath, propertyName);
+				if (byteLength == 0 || pBytes == null)
+					actualByteLength[0] = (uint)stream.Length;
+				else
+					stream.CopyTo(new MemoryStream(pBytes, 0, (int)byteLength));
+				return 0;
+			}
+
+			public int GetBool([ComAliasName("OLE.LPCOLESTR")]string collectionPath, [ComAliasName("OLE.LPCOLESTR")]string propertyName, [ComAliasName("OLE.BOOL")]out int value) {
+				value = inner.GetBoolean(collectionPath, propertyName) ? 1 : 0;
+				return 0;
+			}
+
+			public int GetBoolOrDefault([ComAliasName("OLE.LPCOLESTR")]string collectionPath, [ComAliasName("OLE.LPCOLESTR")]string propertyName, [ComAliasName("OLE.BOOL")]int defaultValue, [ComAliasName("OLE.BOOL")]out int value) {
+				value = inner.GetBoolean(collectionPath, propertyName, defaultValue != 0) ? 1 : 0;
+				return 0;
+			}
+
+			public int GetInt([ComAliasName("OLE.LPCOLESTR")]string collectionPath, [ComAliasName("OLE.LPCOLESTR")]string propertyName, out int value) {
+				value = inner.GetInt32(collectionPath, propertyName);
+				return 0;
+			}
+
+			public int GetInt64([ComAliasName("OLE.LPCOLESTR")]string collectionPath, [ComAliasName("OLE.LPCOLESTR")]string propertyName, out long value) {
+				value = inner.GetInt64(collectionPath, propertyName);
+				return 0;
+			}
+
+			public int GetInt64OrDefault([ComAliasName("OLE.LPCOLESTR")]string collectionPath, [ComAliasName("OLE.LPCOLESTR")]string propertyName, long defaultValue, out long value) {
+				value = inner.GetInt64(collectionPath, propertyName, defaultValue);
+				return 0;
+			}
+
+			public int GetIntOrDefault([ComAliasName("OLE.LPCOLESTR")]string collectionPath, [ComAliasName("OLE.LPCOLESTR")]string propertyName, int defaultValue, out int value) {
+				value = inner.GetInt32(collectionPath, propertyName, defaultValue);
+				return 0;
+			}
+
+			public int GetLastWriteTime([ComAliasName("OLE.LPCOLESTR")]string collectionPath, [ComAliasName("VsShell.SYSTEMTIME")]SYSTEMTIME[] lastWriteTime) {
+				var dt = inner.GetLastWriteTime(collectionPath);
+				lastWriteTime[0].wDay = (ushort)dt.Day;
+				lastWriteTime[0].wDayOfWeek = (ushort)dt.DayOfWeek;
+				lastWriteTime[0].wHour = (ushort)dt.Hour;
+				lastWriteTime[0].wMilliseconds = (ushort)dt.Millisecond;
+				lastWriteTime[0].wMinute = (ushort)dt.Minute;
+				lastWriteTime[0].wMonth = (ushort)dt.Month;
+				lastWriteTime[0].wSecond = (ushort)dt.Second;
+				lastWriteTime[0].wYear = (ushort)dt.Year;
+				return 0;
+			}
+
+			public int GetPropertyCount([ComAliasName("OLE.LPCOLESTR")]string collectionPath, [ComAliasName("OLE.DWORD")]out uint propertyCount) {
+				propertyCount = (uint)inner.GetPropertyCount(collectionPath);
+				return 0;
+			}
+
+			public int GetPropertyName([ComAliasName("OLE.LPCOLESTR")]string collectionPath, [ComAliasName("OLE.DWORD")]uint index, out string propertyName) {
+				propertyName = inner.GetPropertyNames(collectionPath).ElementAt((int)index);
+				return 0;
+			}
+
+			public int GetPropertyType([ComAliasName("OLE.LPCOLESTR")]string collectionPath, [ComAliasName("OLE.LPCOLESTR")]string propertyName, [ComAliasName("Microsoft.VisualStudio.Shell.Interop.VSSETTINGSTYPE")]out uint type) {
+				type = (uint)inner.GetPropertyType(collectionPath, propertyName);
+				return 0;
+			}
+
+			public int GetString([ComAliasName("OLE.LPCOLESTR")]string collectionPath, [ComAliasName("OLE.LPCOLESTR")]string propertyName, out string value) {
+				value = inner.GetString(collectionPath, propertyName);
+				return 0;
+			}
+
+			public int GetStringOrDefault([ComAliasName("OLE.LPCOLESTR")]string collectionPath, [ComAliasName("OLE.LPCOLESTR")]string propertyName, [ComAliasName("OLE.LPCOLESTR")]string defaultValue, out string value) {
+				if (defaultValue == null)
+					value = inner.GetString(collectionPath, propertyName);
+				else
+					value = inner.GetString(collectionPath, propertyName, defaultValue);
+				return 0;
+			}
+
+			public int GetSubCollectionCount([ComAliasName("OLE.LPCOLESTR")]string collectionPath, [ComAliasName("OLE.DWORD")]out uint subCollectionCount) {
+				subCollectionCount = (uint)inner.GetSubCollectionCount(collectionPath);
+				return 0;
+			}
+
+			public int GetSubCollectionName([ComAliasName("OLE.LPCOLESTR")]string collectionPath, [ComAliasName("OLE.DWORD")]uint index, out string subCollectionName) {
+				subCollectionName = inner.GetSubCollectionNames(collectionPath).ElementAt((int)index);
+				return 0;
+			}
+
+			public int GetUnsignedInt([ComAliasName("OLE.LPCOLESTR")]string collectionPath, [ComAliasName("OLE.LPCOLESTR")]string propertyName, [ComAliasName("OLE.DWORD")]out uint value) {
+				value = inner.GetUInt32(collectionPath, propertyName);
+				return 0;
+			}
+
+			public int GetUnsignedInt64([ComAliasName("OLE.LPCOLESTR")]string collectionPath, [ComAliasName("OLE.LPCOLESTR")]string propertyName, out ulong value) {
+				value = inner.GetUInt64(collectionPath, propertyName);
+				return 0;
+			}
+
+			public int GetUnsignedInt64OrDefault([ComAliasName("OLE.LPCOLESTR")]string collectionPath, [ComAliasName("OLE.LPCOLESTR")]string propertyName, ulong defaultValue, out ulong value) {
+				value = inner.GetUInt64(collectionPath, propertyName, defaultValue);
+				return 0;
+			}
+
+			public int GetUnsignedIntOrDefault([ComAliasName("OLE.LPCOLESTR")]string collectionPath, [ComAliasName("OLE.LPCOLESTR")]string propertyName, [ComAliasName("OLE.DWORD")]uint defaultValue, [ComAliasName("OLE.DWORD")]out uint value) {
+				value = inner.GetUInt32(collectionPath, propertyName, defaultValue);
+				return 0;
+			}
+
+			public int PropertyExists([ComAliasName("OLE.LPCOLESTR")]string collectionPath, [ComAliasName("OLE.LPCOLESTR")]string propertyName, [ComAliasName("OLE.BOOL")]out int pfExists) {
+				pfExists = inner.PropertyExists(collectionPath, propertyName) ? 1 : 0;
+				return 0;
+			}
+		}
+
+		class DummyLog : IVsActivityLog {
+			public int LogEntry([ComAliasName("Microsoft.VisualStudio.Shell.Interop.ACTIVITYLOG_ENTRYTYPE")]uint actType, [ComAliasName("Microsoft.VisualStudio.OLE.Interop.LPCOLESTR")]string pszSource, [ComAliasName("Microsoft.VisualStudio.OLE.Interop.LPCOLESTR")]string pszDescription) {
+				return 0;
+			}
+
+			public int LogEntryGuid([ComAliasName("Microsoft.VisualStudio.Shell.Interop.ACTIVITYLOG_ENTRYTYPE")]uint actType, [ComAliasName("Microsoft.VisualStudio.OLE.Interop.LPCOLESTR")]string pszSource, [ComAliasName("Microsoft.VisualStudio.OLE.Interop.LPCOLESTR")]string pszDescription, Guid guid) {
+				return 0;
+			}
+
+			public int LogEntryGuidHr([ComAliasName("Microsoft.VisualStudio.Shell.Interop.ACTIVITYLOG_ENTRYTYPE")]uint actType, [ComAliasName("Microsoft.VisualStudio.OLE.Interop.LPCOLESTR")]string pszSource, [ComAliasName("Microsoft.VisualStudio.OLE.Interop.LPCOLESTR")]string pszDescription, Guid guid, int hr) {
+				return 0;
+			}
+
+			public int LogEntryGuidHrPath([ComAliasName("Microsoft.VisualStudio.Shell.Interop.ACTIVITYLOG_ENTRYTYPE")]uint actType, [ComAliasName("Microsoft.VisualStudio.OLE.Interop.LPCOLESTR")]string pszSource, [ComAliasName("Microsoft.VisualStudio.OLE.Interop.LPCOLESTR")]string pszDescription, Guid guid, int hr, [ComAliasName("Microsoft.VisualStudio.OLE.Interop.LPCOLESTR")]string pszPath) {
+				return 0;
+			}
+
+			public int LogEntryGuidPath([ComAliasName("Microsoft.VisualStudio.Shell.Interop.ACTIVITYLOG_ENTRYTYPE")]uint actType, [ComAliasName("Microsoft.VisualStudio.OLE.Interop.LPCOLESTR")]string pszSource, [ComAliasName("Microsoft.VisualStudio.OLE.Interop.LPCOLESTR")]string pszDescription, Guid guid, [ComAliasName("Microsoft.VisualStudio.OLE.Interop.LPCOLESTR")]string pszPath) {
+				return 0;
+			}
+
+			public int LogEntryHr([ComAliasName("Microsoft.VisualStudio.Shell.Interop.ACTIVITYLOG_ENTRYTYPE")]uint actType, [ComAliasName("Microsoft.VisualStudio.OLE.Interop.LPCOLESTR")]string pszSource, [ComAliasName("Microsoft.VisualStudio.OLE.Interop.LPCOLESTR")]string pszDescription, int hr) {
+				return 0;
+			}
+
+			public int LogEntryHrPath([ComAliasName("Microsoft.VisualStudio.Shell.Interop.ACTIVITYLOG_ENTRYTYPE")]uint actType, [ComAliasName("Microsoft.VisualStudio.OLE.Interop.LPCOLESTR")]string pszSource, [ComAliasName("Microsoft.VisualStudio.OLE.Interop.LPCOLESTR")]string pszDescription, int hr, [ComAliasName("Microsoft.VisualStudio.OLE.Interop.LPCOLESTR")]string pszPath) {
+				return 0;
+			}
+
+			public int LogEntryPath([ComAliasName("Microsoft.VisualStudio.Shell.Interop.ACTIVITYLOG_ENTRYTYPE")]uint actType, [ComAliasName("Microsoft.VisualStudio.OLE.Interop.LPCOLESTR")]string pszSource, [ComAliasName("Microsoft.VisualStudio.OLE.Interop.LPCOLESTR")]string pszDescription, [ComAliasName("Microsoft.VisualStudio.OLE.Interop.LPCOLESTR")]string pszPath) {
+				return 0;
+			}
+		}
+	}
+}
