ec7084662114833b70765508fdb428b27e92ef17
Schabse Laks <Dev@SLaks.net>
2014-01-21 16:57:35 -0500

    Settings: Ignore non-source-level settings

--- Rebracer/Utilities/KnownSettings.cs
+++ Rebracer/Utilities/KnownSettings.cs
@@ -27,13 +27,26 @@ namespace SLaks.Rebracer.Utilities {
 		}
 
 		static readonly IReadOnlyDictionary<SettingsSection, IReadOnlyCollection<string>> skipProperties = new Dictionary<SettingsSection, IReadOnlyCollection<string>>{
-			// Cannot read: Exception occurred. (Exception from HRESULT: 0x80020009 (DISP_E_EXCEPTION))
-			{ new SettingsSection("TextEditor", "C/C++ Specific"), new [] { "IntellisenseOptions" } },
-			// Cannot write: Member not found. (Exception from HRESULT: 0x80020003 (DISP_E_MEMBERNOTFOUND))
-			{ new SettingsSection("TextEditor", "JavaScript Specific"), new [] { "ImplicitReferences" } },
+			{ new SettingsSection("TextEditor", "C/C++ Specific"), new [] { 
+				// Cannot read: Exception occurred. (Exception from HRESULT: 0x80020009 (DISP_E_EXCEPTION))
+				"IntellisenseOptions",
+				// Non-source settings that can vary independently
+				"InactiveCodeOpacityPercent", "CompleteSlashStar", "CompleteParensInRawString", "DisableReferencesResolving"
 
-			// Typo'd setting in some VS builds; ignoring to not give warnings for most people.
-			{ new SettingsSection("TextEditor", "CSharp-Specific"), new []{ "NewLineQueryExpression_EachClause" } }
+			} },
+			{ new SettingsSection("TextEditor", "JavaScript Specific"), new [] {
+				// Cannot write: Member not found. (Exception from HRESULT: 0x80020003 (DISP_E_MEMBERNOTFOUND))
+				"ImplicitReferences",
+				// Non-source settings that can vary independently
+				"OnlyUseTaborEnterToCommit"
+			} },
+
+			{ new SettingsSection("TextEditor", "CSharp-Specific"), new []{ 
+				// Typo'd setting in some VS builds; ignoring to not give warnings for most people.
+				"NewLineQueryExpression_EachClause", 
+				// Non-source settings that can vary independently
+				"Rename_Preview", "EnterOutliningModeOnOpen" 
+			} }
 		};
 		///<summary>Checks whether a specific property should be skipped to to persistence issues.</summary>
 		public static bool ShouldSkip(SettingsSection section, string property) {
