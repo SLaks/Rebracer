63e527a089f196e0883e4e509327c499332c9056
Schabse Laks <Dev@SLaks.net>
2014-11-14 09:23:01 -0500

    Revert "Notifications: Use original WTBB from ViewManager"
    This reverts commit 3f831e728305c3c2faa176e792d06450387c5f07.
    
    Fixes #13
    
    I was wrong; XAML always uses fully-qualified names.
    This therefore broke everything when ViewManager v10
    was in the GAC. I have no idea why it worked before.

--- Rebracer/Notifications/GrowlNotifications.xaml.cs
+++ Rebracer/Notifications/GrowlNotifications.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics.CodeAnalysis;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using Microsoft.VisualStudio.Shell;
 using Task = System.Threading.Tasks.Task;
 
 namespace WpfGrowlNotification {
@@ -48,8 +52,7 @@ namespace WpfGrowlNotification {
 			// but I can only get it from a resource, and
 			// I can't figure out how to find the target.
 			SizeChangedEventHandler sizeChangedHandler = null;
-			sizeChangedHandler = async (s, se) =>
-			{
+			sizeChangedHandler = async (s, se) => {
 				if (se.PreviousSize.Height == 0)
 					return;
 				element.SizeChanged -= sizeChangedHandler;
@@ -59,6 +62,64 @@ namespace WpfGrowlNotification {
 				RemoveNotification((Notification)element.DataContext);
 			};
 			element.SizeChanged += sizeChangedHandler;
+		}
+	}
+	// Stolen from Microsoft.VisualStudio.Shell.ViewManager
+	// This is a version-dependent assembly, so I can't use
+	// it at all.
+	//http://twitter.com/jaredpar/status/415661754979717120
+	public class TitleBarButton : Button {
+		// Removed unused IsChecked property.
+		public static readonly DependencyProperty PressedBackgroundProperty = DependencyProperty.Register("PressedBackground", typeof(Brush), typeof(TitleBarButton));
+		public static readonly DependencyProperty PressedBorderBrushProperty = DependencyProperty.Register("PressedBorderBrush", typeof(Brush), typeof(TitleBarButton));
+		public static readonly DependencyProperty PressedBorderThicknessProperty = DependencyProperty.Register("PressedBorderThickness", typeof(Thickness), typeof(TitleBarButton));
+		public static readonly DependencyProperty HoverBackgroundProperty = DependencyProperty.Register("HoverBackground", typeof(Brush), typeof(TitleBarButton));
+		public static readonly DependencyProperty HoverBorderBrushProperty = DependencyProperty.Register("HoverBorderBrush", typeof(Brush), typeof(TitleBarButton));
+		public static readonly DependencyProperty HoverBorderThicknessProperty = DependencyProperty.Register("HoverBorderThickness", typeof(Thickness), typeof(TitleBarButton));
+		public static readonly DependencyProperty GlyphForegroundProperty = DependencyProperty.Register("GlyphForeground", typeof(Brush), typeof(TitleBarButton));
+		public static readonly DependencyProperty HoverForegroundProperty = DependencyProperty.Register("HoverForeground", typeof(Brush), typeof(TitleBarButton));
+		public static readonly DependencyProperty PressedForegroundProperty = DependencyProperty.Register("PressedForeground", typeof(Brush), typeof(TitleBarButton));
+
+		public Brush PressedBackground {
+			get { return (Brush)GetValue(PressedBackgroundProperty); }
+			set { SetValue(PressedBackgroundProperty, value); }
+		}
+		public Brush PressedBorderBrush {
+			get { return (Brush)GetValue(PressedBorderBrushProperty); }
+			set { SetValue(PressedBorderBrushProperty, value); }
+		}
+		public Thickness PressedBorderThickness {
+			get { return (Thickness)GetValue(PressedBorderThicknessProperty); }
+			set { SetValue(PressedBorderThicknessProperty, value); }
+		}
+		public Brush HoverBackground {
+			get { return (Brush)GetValue(HoverBackgroundProperty); }
+			set { SetValue(HoverBackgroundProperty, value); }
+		}
+		public Brush HoverBorderBrush {
+			get { return (Brush)GetValue(HoverBorderBrushProperty); }
+			set { SetValue(HoverBorderBrushProperty, value); }
+		}
+		public Thickness HoverBorderThickness {
+			get { return (Thickness)GetValue(HoverBorderThicknessProperty); }
+			set { SetValue(HoverBorderThicknessProperty, value); }
+		}
+		public Brush GlyphForeground {
+			get { return (Brush)GetValue(GlyphForegroundProperty); }
+			set { SetValue(GlyphForegroundProperty, value); }
+		}
+		public Brush HoverForeground {
+			get { return (Brush)GetValue(HoverForegroundProperty); }
+			set { SetValue(HoverForegroundProperty, value); }
+		}
+		public Brush PressedForeground {
+			get { return (Brush)GetValue(PressedForegroundProperty); }
+			set { SetValue(PressedForegroundProperty, value); }
+		}
+
+		[SuppressMessage("Microsoft.Performance", "CA1810:InitializeReferenceTypeStaticFieldsInline")]
+		static TitleBarButton() {
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(TitleBarButton), new FrameworkPropertyMetadata(typeof(TitleBarButton)));
 		}
 	}
 }
